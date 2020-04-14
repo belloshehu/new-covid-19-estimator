8a2d89b25e9d89f7fff01bd6793314dc20d794c9
ardesp <belloshehu1@gmail.com>
2020-04-14 23:12:50 +0100

    flask app code taken away

--- src/estimator.py
+++ src/estimator.py
@@ -1,17 +1,10 @@
 import  json
 from data import data
-from flask import Flask,g, jsonify, request
-from dicttoxml import dicttoxml
 from math import trunc
 import logging
-import time
 request_log = {}
 app = Flask(__name__)
 
-
-def log_request_response(request_method,url,status,duration):
-  logging.basicConfig(filename='serverlog.log', level=logging.INFO)
-  logging.info("{}\t\t{} \t\t {}\t\t{}".format(request_method,url,status,duration))
 #returns duration for estimation
 def get_duration(data):
 
@@ -93,48 +86,3 @@ def estimator(data):
     result3 = challenge3_soluton(data)
     output_data = result3
     return output_data
-
-@app.route("/api/v1/on-covid-19", methods=["POST"])
-def default_api():
-    return jsonify(estimator(data))
-
-@app.route("/api/v1/on-covid-19/json", methods=["POST"])
-def json_api():
-    log_request_response(request.method,"/api/v1/on-covid-19/json",200, 20)
-    return jsonify(estimator(data))
-
-@app.route("/api/v1/on-covid-19/xml", methods=["POST"])
-def xml_api():
-    print(request.path)
-    print(request.url)
-    xml =dicttoxml(estimator(data))
-    return xml
-
-@app.route("/api/v1/on-covid-19/log", methods=["GET"])
-def send_log():
-  message =""
-  with open("log.txt","r") as file_ref:
-    lines = file_ref.readlines()
-    for line in lines:
-      message =message+line+"<br>"
-  return message
-
-@app.before_request
-def commence_timing():
-  g.start_time =time.time()
-
-
-@app.after_request
-def stop_timing(response):
-  duration = round(time.time()-g.start_time,2)
-  status =response.status_code
-  method_type = request.method
-  url = request.path
-  with open('log.txt', 'a') as file_ref:
-    print("{}\t\t{}\t\t{}\t\t{}ms".format(method_type,url,status,duration), file=file_ref)
-  return response
-
-
-if __name__=="__main__":
-    app.run(debug=True)
-
